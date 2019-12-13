2ac7fc53f3fbb1e7c7dfbdc55977a9f51ed116c5
ernestosotelo <ernesto.sotelo@hotmail.com>
2019-12-12 20:27:06 -0500

    Updates

---
+++ precache-manifest.fbf422554329fb425fd8615f8a526fca.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "577249356d2b29c13ada7e705cf28c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a0edf51c50c0cf63eb",
+    "url": "/static/css/main.863425fa.chunk.css"
+  },
+  {
+    "revision": "64cbd6dadfc5d5e0f681",
+    "url": "/static/js/2.b92f3aa1.chunk.js"
+  },
+  {
+    "revision": "b5a0edf51c50c0cf63eb",
+    "url": "/static/js/main.ef5ad7f1.chunk.js"
+  },
+  {
+    "revision": "785f1f09ce41a4a48751",
+    "url": "/static/js/runtime-main.37fa2dab.js"
+  },
+  {
+    "revision": "1609d42bec02e9a084539c1cda2f22d9",
+    "url": "/static/media/account.1609d42b.svg"
+  },
+  {
+    "revision": "d6f1ae3ee7c80aa65063db6a27b5f31b",
+    "url": "/static/media/awards.d6f1ae3e.svg"
+  },
+  {
+    "revision": "870bd6b3b34b56e457db832201a925d1",
+    "url": "/static/media/back.870bd6b3.svg"
+  },
+  {
+    "revision": "e5bc2e1da189152410ec33486859290f",
+    "url": "/static/media/card-left.e5bc2e1d.svg"
+  },
+  {
+    "revision": "8b6fa08f28754418a9d0678c90e17eae",
+    "url": "/static/media/card-right.8b6fa08f.svg"
+  },
+  {
+    "revision": "8a1e150f74bb2f45451c7e8772d6cb66",
+    "url": "/static/media/icon.8a1e150f.svg"
+  },
+  {
+    "revision": "4610a8371556ce60ae6416b7f48ef0f7",
+    "url": "/static/media/intro-flavell.4610a837.svg"
+  },
+  {
+    "revision": "8184dbce1657f18f93ad1086c12980c4",
+    "url": "/static/media/intro-meta-components.8184dbce.svg"
+  },
+  {
+    "revision": "0266d14329754908d873020e85be7c9f",
+    "url": "/static/media/intro-thinking.0266d143.svg"
+  },
+  {
+    "revision": "1d1e3962a5ea36d5c3fcef9d079ed115",
+    "url": "/static/media/lock.1d1e3962.svg"
+  },
+  {
+    "revision": "7f7dc69fbdc0f220a4831911ac95cebc",
+    "url": "/static/media/logo.7f7dc69f.svg"
+  },
+  {
+    "revision": "62e7f4792e80e5e4db66733dbe199688",
+    "url": "/static/media/measuring-1.62e7f479.svg"
+  },
+  {
+    "revision": "dae15bd48478b041bf231fa079e9c1e5",
+    "url": "/static/media/measuring-2.dae15bd4.svg"
+  },
+  {
+    "revision": "06a55900c82ba94701980311fc7d5fbe",
+    "url": "/static/media/metacognition.06a55900.svg"
+  },
+  {
+    "revision": "6331c61df562d0fb9fe7bf6b3b0b3cba",
+    "url": "/static/media/modules.6331c61d.svg"
+  },
+  {
+    "revision": "013391fc126b5ada0f88c45383c6b50a",
+    "url": "/static/media/nextPage.013391fc.svg"
+  },
+  {
+    "revision": "e7c6789b801d0e3477aafd04e650e309",
+    "url": "/static/media/pentagon.e7c6789b.svg"
+  },
+  {
+    "revision": "f69b6053a8615dd0e36be9f8de2ef3fb",
+    "url": "/static/media/prevPage.f69b6053.svg"
+  },
+  {
+    "revision": "f94a87425e35d84b7e08abf699004dbe",
+    "url": "/static/media/question.f94a8742.svg"
+  },
+  {
+    "revision": "e0468331a7a45acf72a867222f2f6718",
+    "url": "/static/media/rights.e0468331.svg"
+  },
+  {
+    "revision": "1979f806e3b3598003b589c334375527",
+    "url": "/static/media/stats.1979f806.svg"
+  }
+]);
